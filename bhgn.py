65d421d9cbe6a0235731820341fbbfee5f550aec
Sanidhya Sahu <98507281+sanidhya-sahu@users.noreply.github.com>
2023-04-16 13:57:27 +0530

    still working

--- bhgn.py
+++ bhgn.py
@@ -1,31 +1,19 @@
-string_list = [] # empty list to store the strings
-a = input()
-b = a.capitalize()
-print(b)
-search_words = b.split()
-i=0
-with open("materialcode.txt", "r") as file:
-    lines = file.readlines()
+import tkinter as tk
 
-for line in lines:
-    for word in search_words:
-        if word in line:
-            print(line)
-            string_name = f"string_{i + 1}"  # create a variable name with a unique number
-            string_value = f"{line}"  # create the string value
-            locals()[string_name] = string_value  # dynamically create the variable and assign the string value
-            string_list.append(locals()[string_name])  # add the variable to the list
-            i+=1
-            break
-# n = 5 # number of strings to create
-#
-# for i in range(n):
-#     string_name = f"string_{i+1}" # create a variable name with a unique number
-#     string_value = f"This is string {i+1}" # create the string value
-#     locals()[string_name] = string_value # dynamically create the variable and assign the string value
-#     string_list.append(locals()[string_name]) # add the variable to the list
-# # print the list of strings
-print(string_list)
-print(locals()["string_1"])
-print(locals()["string_6"])
-print(locals()["string_4"])
+def get_button_text(button_id):
+    """Function to retrieve text of a button with a given identifier"""
+    button_text = buttons[button_id]['text']
+    print(f"The text of button {button_id} is: {button_text}")
+
+def create_buttons():
+    """Function to create a group of buttons with unique identifiers"""
+    global buttons
+    buttons = {}
+    for i in range(5):
+        button = tk.Button(root, text=f"Button {i+1}", command=lambda button_id=i: get_button_text(button_id))
+        button.pack()
+        buttons[i] = button
+    print(buttons)
+root = tk.Tk()
+create_buttons()
+root.mainloop()
--- ckbc.py
+++ ckbc.py
@@ -1,8 +1,32 @@
 from tkinter import *
 from tkinter import ttk
 import dataIO
+ubtn1={}
+ubtn2={}
+ubtn3={}
+ubtn4={}
+# button_id=0
+def get_button_text1(button_id):
+    """Function to retrieve text of a button with a given identifier"""
+    # print(ubtn)
+    btn_text = ubtn1[button_id]['text']
+    print(btn_text)
+def get_button_text2(button_id):
+    """Function to retrieve text of a button with a given identifier"""
+    # print(ubtn)
+    btn_text = ubtn2[button_id]['text']
+    print(btn_text)
+def get_button_text3(button_id):
+    """Function to retrieve text of a button with a given identifier"""
+    # print(ubtn)
+    btn_text = ubtn3[button_id]['text']
+    print(btn_text)
 
-
+def get_button_text4(button_id):
+        """Function to retrieve text of a button with a given identifier"""
+        # print(ubtn)
+        btn_text = ubtn4[button_id]['text']
+        print(btn_text)
 # from home import *
 def to1():
     top = Toplevel()
@@ -28,15 +52,20 @@ def to1():
 
         for word in search_words:
             if word in line:
+                global buttons
                 # print(line)
+                buttons = {}
+
                 string_name = f"string1_{i + 1}"  # create a variable name with a unique number
                 string_value = f"{line}"  # create the string value
                 locals()[string_name] = string_value  # dynamically create the variable and assign the string value
                 string_list.append(locals()[string_name])  # add the variable to the list
                 button_text = locals()[f"string1_{i + 1}"]  # create the text for the button
-                button = ttk.Button(top, text=button_text)  # create the button with the text
+                button = ttk.Button(top, text=button_text,command=lambda button_id=i: get_button_text1(button_id))  # create the button with the text
                 button.pack()  # add the button to the GUI
-
+                buttons[i] = button
+                # print(button)
+                ubtn1.update(buttons)
                 i += 1
                 break
     # print(string_list)
@@ -70,8 +99,11 @@ def to2():
                 locals()[string_name] = string_value  # dynamically create the variable and assign the string value
                 string_list.append(locals()[string_name])  # add the variable to the list
                 button_text = locals()[f"string2_{i + 1}"]  # create the text for the button
-                button = ttk.Button(top, text=button_text)  # create the button with the text
+                button = ttk.Button(top, text=button_text,command=lambda button_id=i: get_button_text2(button_id))  # create the button with the text
                 button.pack()  # add the button to the GUI
+                buttons[i] = button
+                # print(button)
+                ubtn2.update(buttons)
                 i += 1
                 # MAKE SEPrATE INPUTS FOr SEPArATE BUTTONS
                 break
@@ -106,8 +138,11 @@ def to3():
                 locals()[string_name] = string_value  # dynamically create the variable and assign the string value
                 string_list.append(locals()[string_name])  # add the variable to the list
                 button_text = locals()[f"string3_{i + 1}"]  # create the text for the button
-                button = ttk.Button(top, text=button_text)  # create the button with the text
+                button = ttk.Button(top, text=button_text,command=lambda button_id=i: get_button_text3(button_id))  # create the button with the text
                 button.pack()  # add the button to the GUI
+                buttons[i] = button
+                # print(button)
+                ubtn3.update(buttons)
                 i += 1
                 break
     # print(string_list)
@@ -141,8 +176,11 @@ def to4():
                 locals()[string_name] = string_value  # dynamically create the variable and assign the string value
                 string_list.append(locals()[string_name])  # add the variable to the list
                 button_text = locals()[f"string4_{i + 1}"]  # create the text for the button
-                button = ttk.Button(top, text=button_text)  # create the button with the text
+                button = ttk.Button(top, text=button_text,command=lambda button_id=i: get_button_text4(button_id))  # create the button with the text
                 button.pack()  # add the button to the GUI
+                buttons[i] = button
+                # print(button)
+                ubtn4.update(buttons)
                 i += 1
                 break
     # print(string_list)
